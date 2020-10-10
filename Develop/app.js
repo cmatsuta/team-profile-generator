cb33cce660a45dfd1a1188f7e481ca1e489d3845
cmatsuta <cmatsuta@jbs.com>
2020-10-10 15:58:54 -0700

    Initial update

--- Develop/app.js
+++ Develop/app.js
@@ -13,6 +13,25 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+const getherInformation = (role) => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Enter your name",
+            required: true,
+        },
+        {
+            type: "list",
+            name: "role",
+            message: "What is your role?",
+            choices: ["Manager", "Engineer", "Intern"]
+        },
+    ])
+}
+
+
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
